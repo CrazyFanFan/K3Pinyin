bac1fa1efee900a5603cf541416a5bda457c30ee
Crazy凡 <kongkk@fxiaoke.com>
2018-02-25 00:59:10 +0800

    add Example target

---
+++ K3Pinyin_Demo/ExampleViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ExampleViewController.swift
+//  K3Pinyin_Demo
+//
+//  Created by Crazy凡 on 2018/2/25.
+//  Copyright © 2018年 Crazy凡. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
