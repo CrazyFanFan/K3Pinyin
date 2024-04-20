14b6ec8163e8a829cc065f9c9eec34fad48a512a
kongkaikai <kongkaikai@bytedance.com>
2024-04-21 01:03:52 +0800

    SPM: platforms
    Change-Id: I10372870beb4d46359d7cb676e96ef7f674033ff

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "K3Pinyin",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v4)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
